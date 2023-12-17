88decfb2d991126d82a310bdb30a44774895027d
Christian Legge <christian@christianlegge.dev>
2023-12-17 16:29:34 -0500

    day 7 part 1

--- lib/days/day07.ex
+++ lib/days/day07.ex
@@ -11,9 +11,20 @@ defmodule Aoc2022.Day07 do
         instr == nil ->
           dirs
 
+        String.starts_with?(instr, "$ cd ..") ->
+          IO.inspect(cwd)
+          parent_dir = cwd |> String.split("/") |> Enum.drop(-2) |> Enum.join("/")
+
+          IO.inspect(parent_dir)
+          process_instructions(rest, dirs, parent_dir <> "/")
+
+        String.starts_with?(instr, "$ cd /") ->
+          process_instructions(rest, dirs, "/")
+
         String.starts_with?(instr, "$ cd") ->
           next_dir = instr |> String.split() |> List.last()
-          process_instructions(rest, dirs, next_dir)
+
+          process_instructions(rest, dirs, cwd <> next_dir <> "/")
 
         String.starts_with?(instr, "$ ls") ->
           {listing, rest} = Enum.split_while(rest, fn l -> !String.starts_with?(l, "$") end)
@@ -22,7 +33,11 @@ defmodule Aoc2022.Day07 do
 
           dir_contents =
             List.foldl(listing, %{}, fn [sizetype, name], acc ->
-              Map.put(acc, name, sizetype)
+              Map.put(
+                acc,
+                name,
+                sizetype
+              )
             end)
 
           dirs = Map.merge(dirs, %{cwd => dir_contents})
@@ -34,20 +49,22 @@ defmodule Aoc2022.Day07 do
   def get_parent_dir(cwd, dirs) do
   end
 
-  def get_object_size({name, sizetype}, dirs) do
+  def get_object_size({name, sizetype}, parent, dirs) do
+    IO.inspect({name, sizetype})
+
     case sizetype do
-      "dir" -> get_dir_size(name, dirs)
-      _ -> Integer.parse(sizetype) |> elem(0)
+      "dir" -> get_dir_size(parent <> name <> "/", dirs)
+      _ -> sizetype |> Integer.parse() |> elem(0)
     end
   end
 
   def get_dir_size(dir, dirs) do
     contents = dirs[dir] |> Map.to_list()
-    contents |> Enum.map(fn obj -> get_object_size(obj, dirs) end) |> Enum.sum()
+    contents |> Enum.map(fn obj -> get_object_size(obj, dir, dirs) end) |> Enum.sum()
   end
 
   def solve(input) do
-    instrs = String.split(input, "\n")
+    instrs = String.split(input, "\n") |> Enum.filter(fn s -> String.trim(s) != "" end)
     dirs = process_instructions(instrs, %{}, "/")
     IO.inspect(dirs)
     sizes = Map.keys(dirs) |> Enum.map(&get_dir_size(&1, dirs))
