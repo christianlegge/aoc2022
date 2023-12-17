a510f093b9d3a3bc879a43ffc2203cef9196cfaf
Christian Legge <christian@christianlegge.dev>
2023-12-17 03:21:55 -0500

    day 7 boilerplate

---
+++ lib/days/day07.ex
@@ -0,0 +1,4 @@
+defmodule Aoc2022.Day07 do
+  def solve(input) do
+  end
+end
--- test/aoc2022_test.exs
+++ test/aoc2022_test.exs
@@ -68,7 +68,34 @@ move 2 from 2 to 1
 move 1 from 1 to 2", Aoc2022.Day05)
   end
 
+  @tag :skip
   test "day 06" do
     run_day("06", "mjqjpqmgbljsphdztnvjfqwrcgsmlb", Aoc2022.Day06)
+  end
+
+  test "day 07" do
+    run_day("07", "$ cd /
+$ ls
+dir a
+14848514 b.txt
+8504156 c.dat
+dir d
+$ cd a
+$ ls
+dir e
+29116 f
+2557 g
+62596 h.lst
+$ cd e
+$ ls
+584 i
+$ cd ..
+$ cd ..
+$ cd d
+$ ls
+4060174 j
+8033020 d.log
+5626152 d.ext
+7214296 k", Aoc2022.Day07)
   end
 end
